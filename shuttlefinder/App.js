680f04839908b9380c409ad11d9fe0b85ca745d1
akshit792 <akshit792@gmail.com>
2019-09-21 16:28:47 -0400

    Added Welcome Screen #11

--- shuttlefinder/App.js
+++ shuttlefinder/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import Welcome from './Components/Welcome';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -9,11 +10,7 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <Welcome />
     );
   }
 }
---
+++ shuttlefinder/Components/Welcome.js
@@ -0,0 +1,46 @@
+import React, {
+    Component
+} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    AppRegistry
+} from 'react-native';
+
+export default class Welcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            welcomemessage: 'Welcome to Shuttle Finder',
+            location: "Enter Location"
+        }
+    }
+
+    static defaultprops = {
+        welcomemessage: 'Welcome to Shuttle Finder',
+        location: "Enter Location"
+    }
+
+    render() {
+        return ( <View style = {styles.container} >
+            <Text style = {{padding: 50, fontSize: 25}} > {this.state.welcomemessage} </Text>  
+            <TextInput style = {{padding: 70, fontSize: 15}} > {
+                this.state.location
+            } </TextInput> 
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'powderblue',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+});
+AppRegistry.registerComponent('Welcome', () => Welcome);
